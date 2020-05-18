1715fa29754fa983d003b25bbd4af70080720b0e
Greg Pasquariello <greg@pasq.net>
2020-05-18 16:33:56 -0500

    Added command sequences.

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import {
   assertEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import { exec, IExecResponse, OutputMode } from "./mod.ts";
+import { exec, execSequence, IExecResponse, OutputMode } from "./mod.ts";
 
 Deno.test({
   name: "Testing Simple Commands",
@@ -37,5 +37,21 @@ Deno.test({
     );
     assertEquals(response.output, "");
     assertEquals(response.status.code, 1);
+  },
+});
+
+Deno.test({
+  name: "Testing Command Sequences",
+  async fn(): Promise<void> {
+    let response = await execSequence([
+      "echo Hello World",
+      "ls -l",
+      "cat helloworld.txt",
+    ], { output: OutputMode.None, continueOnError: false });
+
+    assertEquals(response.length, 3);
+    response.forEach((r: IExecResponse) => {
+      assertEquals(r.status.code, 0);
+    });
   },
 });
